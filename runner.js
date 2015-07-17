1dc0adf4f586167112404c71733fe9896c3fb83b
Manthan The <Manthan.Dave@capitalone.com>
2015-07-17 15:04:15 +0100

    Now finds ways around obstacles by navigating through in angles.

--- runner.js
+++ runner.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 var sight = 2;
-var maxSteps = 10;
+var maxSteps = 5;
 
 run(sight, maxSteps);
 
@@ -9,29 +9,33 @@ function run (sight, maxSteps) {
     var location = {x: 0, y: 0}
 
     var steps = 0;
-    var angle = 90;  // Starts by going upwards.
+    var angle = 0;  // Starts by going upwards.
 
     var mysense = sense(sight);
     var travelHistory = [];
-    mysense.obstructions.push({x: 0, y: 5});    // Setting obstacle
-    mysense.obstructions.push({x: 1, y: 5});    // Setting obstacle
-    mysense.obstructions.push({x: 2, y: 5});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 0});    // Setting obstacle
+    mysense.obstructions.push({x: 4, y: 1});    // Setting obstacle
+    mysense.obstructions.push({x: 3, y: 1});    // Setting obstacle
     mysense.obstructions.push({x: -1, y: 5});    // Setting obstacle
 
     var obstructed = undefined;
-    while(steps < maxSteps && !obstructed) {
+    while(steps < maxSteps) {
         declarePosition(location.x, location.y);
-        var obstructionFound = mysense.findObstruction(location.x, location.y);
+        var obstructionFound = mysense.findObstruction(angle, location.x, location.y);
         if(obstructionFound == false) {
             console.log("Whoohoo! No obstruction found. Moving on!");
             travelHistory.push(location);
             location = move(angle, location.x, location.y); // Prefer positive vertical movement
         } else {
-            console.log("Obstruction found. Stopping now.");
+            console.log("Obstruction found. Will try to go around it.");
             console.log(obstructionFound);
-            obstructed = obstructionFound;
-            break;
+
+            angle++;    // prefers clockwise rotation to avoid obstructions.
+
+            console.log("Rotating to angle: " + angle);
+            continue;
         }
+
         steps++;
     }
 
@@ -41,12 +45,12 @@ function run (sight, maxSteps) {
 function move (angle, currentX, currentY) {
     var x = currentX;
     var y = currentY;
-    var a = angle;
+    var a = angle * (Math.PI / 180);
 
     var targetX = x + (1 * Math.cos(a));
-    targetX = Math.ceil(targetX);
+    targetX = Math.round(targetX);
     var targetY = y + (1 * Math.sin(a));
-    targetY = Math.ceil(targetY);
+    targetY = Math.round(targetY);
 
     return {x: targetX, y: targetY};
 }
@@ -60,48 +64,35 @@ function sense (radius) {
     sense.radius = radius;
     sense.obstructions = [];
     sense.recentObstructions = [];  // TODO: Cache recently encountered obstructions here.
-    sense.findObstruction = function (currentX, currentY) {
+    sense.findObstruction = function (angle, currentX, currentY) {
         // TODO: Start from 0 and pulsate outward, sweeping for obstruction in every pixel until the size of radius.
         var x = currentX;
         var y = currentY;
+        var a = angle * (Math.PI / 180);
         var affectingObstructions = [];
 
         for(var i = 0; i < sense.obstructions.length; i++) {
             // TODO: Rather than going through every obstruction in the world, check based on current angle and current target pixel for greater efficiency.
             var obstruction = sense.obstructions[i];
-            var targetLocation = {x: x, y: y};
 
-            for(var a = 0; a < 360; a++) {  // Sweeping for obstructions in 360 angle.
-                var targetX = x + (sense.radius * Math.cos(a));
-                targetX = Math.floor(targetX);
-                var targetY = y + (sense.radius * Math.sin(a));
-                targetY = Math.floor(targetY);
+            if(obstruction.x == x && obstruction.y == y) {
+                return obstruction;
+            }
 
-                // var cTargetLocation = {x: targetX, y: targetY};
-                // if(cTargetLocation.x != targetLocation.x || cTargetLocation.y != targetLocation.y) {
-                //     console.log(cTargetLocation);
-                // }
-                // targetLocation = cTargetLocation;
+            for(var sight = 1; sight <= sense.radius; sight++) {
+                var targetX = x + (sight * Math.cos(a));
+                targetX = Math.round(targetX);
+                var targetY = y + (sight * Math.sin(a));
+                targetY = Math.round(targetY);
 
-                // if(a < 10) {
-                //     console.log("angle: " + a + " targets: ");
-                //     console.log({x: targetX, y: targetY});
-                // }
+                console.log("For sight " + sight + ", looking at angle " + a + " " + targetX + ", " + targetY);
 
-                if((obstruction.x == targetX && obstruction.y == targetY)) {
-                    if(affectingObstructions.length > 0) {
-                        var lastAddedObstruction = affectingObstructions[affectingObstructions.length - 1].obstruction;
-                        if(lastAddedObstruction.x != obstruction.x || lastAddedObstruction.y != obstruction.y) {
-                            affectingObstructions.push({ obstruction: obstruction, angle: a });
-                        }
-                    } else {
-                        affectingObstructions.push({ obstruction: obstruction, angle: a });
-                    }
+                if(obstruction.x == targetX && obstruction.y == targetY) {
+                    return obstruction;
                 }
             }
         }
-
-        return (affectingObstructions.length == 0) ? false : affectingObstructions;
+        return false;
     }
 
     return sense;
