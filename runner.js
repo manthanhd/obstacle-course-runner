b3b49edbc74e0a722e427a2046d5087308ca9a85
Manthan Dave <manthanhd@live.com>
2015-07-18 22:36:48 +0100

    Added g-maze for benchmark + can now remember and recall locations.

--- runner.js
+++ runner.js
@@ -1,45 +1,148 @@
 #!/usr/bin/node
 
 var sight = 2;
-var maxSteps = 5;
+var maxSteps = 20;
 
 run(sight, maxSteps);
 
 function run (sight, maxSteps) {
-    var location = {x: 0, y: 0}
+    var location = {x: 7, y: 14};
 
     var steps = 0;
     var angle = 0;  // Starts by going upwards.
 
     var mysense = sense(sight);
-    var travelHistory = [];
-    mysense.obstructions.push({x: 5, y: 0});    // Setting obstacle
-    mysense.obstructions.push({x: 4, y: 1});    // Setting obstacle
-    mysense.obstructions.push({x: 3, y: 1});    // Setting obstacle
-    mysense.obstructions.push({x: -1, y: 5});    // Setting obstacle
+    var mymemory = memory();
+
+    mysense.obstructions.push({x: 9, y: 21});    // Setting obstacle
+    mysense.obstructions.push({x: 8, y: 21});    // Setting obstacle
+    mysense.obstructions.push({x: 8, y: 22});    // Setting obstacle
+    mysense.obstructions.push({x: 7, y: 21});    // Setting obstacle
+    mysense.obstructions.push({x: 7, y: 22});    // Setting obstacle
+    mysense.obstructions.push({x: 6, y: 21});    // Setting obstacle
+    mysense.obstructions.push({x: 6, y: 22});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 21});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 20});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 19});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 18});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 17});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 16});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 15});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 14});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 13});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 12});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 11});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 10});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 9});    // Setting obstacle
+    mysense.obstructions.push({x: 5, y: 8});    // Setting obstacle
+    mysense.obstructions.push({x: 6, y: 8});    // Setting obstacle
+    mysense.obstructions.push({x: 7, y: 8});    // Setting obstacle
+    mysense.obstructions.push({x: 8, y: 8});    // Setting obstacle
+    mysense.obstructions.push({x: 9, y: 8});    // Setting obstacle
+    mysense.obstructions.push({x: 9, y: 9});    // Setting obstacle
+    mysense.obstructions.push({x: 9, y: 10});    // Setting obstacle
+    mysense.obstructions.push({x: 9, y: 11});    // Setting obstacle
+    mysense.obstructions.push({x: 9, y: 12});    // Setting obstacle
+    mysense.obstructions.push({x: 9, y: 13});    // Setting obstacle
+    mysense.obstructions.push({x: 9, y: 14});    // Setting obstacle
+    mysense.obstructions.push({x: 9, y: 15});    // Setting obstacle
+    mysense.obstructions.push({x: 8, y: 15});    // Setting obstacle
+    mysense.obstructions.push({x: 7, y: 15});    // Setting obstacle
 
     var obstructed = undefined;
+    var rotation = true;    // true for counter-clockwise and false for clockwise.
     while(steps < maxSteps) {
-        declarePosition(location.x, location.y);
+        if(angle >= 360) {
+            angle = 360 - angle;
+        }
+
+        // declarePosition(location.x, location.y);
         var obstructionFound = mysense.findObstruction(angle, location.x, location.y);
         if(obstructionFound == false) {
-            console.log("Whoohoo! No obstruction found. Moving on!");
-            travelHistory.push(location);
+            // console.log("Whoohoo! No obstruction found. Moving on!");
+            mymemory.remember(location);
+
             location = move(angle, location.x, location.y); // Prefer positive vertical movement
+
+            declarePosition(angle, location.x, location.y);
         } else {
-            console.log("Obstruction found. Will try to go around it.");
-            console.log(obstructionFound);
+            // console.log("Obstruction found. Will try to go around it.");
+            // console.log(obstructionFound);
 
-            angle++;    // prefers clockwise rotation to avoid obstructions.
+            var a = angle;
+            var foundNew = false;
+            for(var i = 0; i < 360; i++) {
+                if(a >= 360) {
+                    a = 360 - a;
+                } else if (a <= 0) {
+                    a = 360;
+                }
 
-            console.log("Rotating to angle: " + angle);
+                if(rotation == true) {
+                    a++;
+                } else {
+                    a--;
+                }
+
+                var l = move(a, location.x, location.y);
+                var iRemember = mymemory.doIRemember(l);
+                if(iRemember == false && mysense.findObstruction(a, location.x, location.y) == false) {
+                    // console.log("New location!");
+                    // console.log(l);
+                    angle = a;
+                    foundNew = true;
+                    break;
+                }
+            }
+
+            if(foundNew == false) {
+                rotation = (rotation == true) ? false : true;
+            }
+            // angle++;    // prefers clockwise rotation to avoid obstructions.
+            // angle += 5;
+
+            // console.log("Rotating to angle: " + angle);
             continue;
         }
 
         steps++;
     }
 
-    console.log("Travelled " + travelHistory.length + " steps.");
+    console.log("Travelled " + steps + " steps.");
+    console.log("Known locations:");
+    console.log(mymemory.locations);
+}
+
+function memory () {
+    var memory = {};
+    memory.locations = [];
+    memory.remember = function(locationToRemember) {
+        var found = false;
+        for(var i = 0; i < memory.locations.length; i++) {
+            var location = memory.locations[i];
+            if(location.x == locationToRemember.x && location.y == locationToRemember.y) {
+                found = true;
+                return;
+            }
+        }
+
+        if(found == false) {
+            memory.locations.push(locationToRemember);
+        }
+    };
+
+    memory.doIRemember = function(locationToRetrieve) {
+        for(var i = 0; i < memory.locations.length; i++) {
+            var location = memory.locations[i];
+            if(location.x == locationToRetrieve.x && location.y == locationToRetrieve.y) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    return memory;
 }
 
 function move (angle, currentX, currentY) {
@@ -55,8 +158,8 @@ function move (angle, currentX, currentY) {
     return {x: targetX, y: targetY};
 }
 
-function declarePosition (x, y) {
-    console.log("I'm at " + x + ", " + y);
+function declarePosition (angle, x, y) {
+    console.log("I'm at " + x + ", " + y + " pointing " + angle + " degrees.");
 }
 
 function sense (radius) {
@@ -85,7 +188,7 @@ function sense (radius) {
                 var targetY = y + (sight * Math.sin(a));
                 targetY = Math.round(targetY);
 
-                console.log("For sight " + sight + ", looking at angle " + a + " " + targetX + ", " + targetY);
+                // console.log("For sight " + sight + ", looking at angle " + a + " " + targetX + ", " + targetY);
 
                 if(obstruction.x == targetX && obstruction.y == targetY) {
                     return obstruction;
